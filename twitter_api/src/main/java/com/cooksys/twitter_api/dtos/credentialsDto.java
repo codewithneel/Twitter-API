dd6a10a4e2e4b17f7efd37f8753674bf18306280
Gino Edgar Ngomeni <gngome1@wgu.edu>
2024-11-19 10:30:15 -0800

    credentials, profile, user, usermapper

---
+++ twitter_api/src/main/java/com/cooksys/twitter_api/dtos/credentialsDto.java
@@ -0,0 +1,14 @@
+package com.cooksys.twitter_api.dtos;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class credentialsDto {
+
+    private String username;
+    private String password;
+
+}
---
+++ twitter_api/src/main/java/com/cooksys/twitter_api/entities/User.java
@@ -0,0 +1,35 @@
+package com.cooksys.twitter_api.entities;
+
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String password;
+
+    @GeneratedValue
+    private Timestamp timestamp;
+
+    private boolean deleted;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    @ManyToMany
+    @JoinTable(name="followers_following", joinColumns = @JoinColumn(name="followers_id"), inverseJoinColumns = @JoinColumn(name="following_id"))
+    private List<User> followers;
+}
