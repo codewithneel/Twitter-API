3e021175784b6314f8c1dbbc19b264f06c3429e5
nyy212 <nyy212@vols.utk.edu>
2024-11-19 11:44:50 -0600

    hashtag infrastructure and error handling

---
+++ twitter_api/src/main/java/com/cooksys/twitter_api/mappers/HashtagMapper.java
@@ -0,0 +1,13 @@
+package com.cooksys.twitter_api.mappers;
+
+import org.mapstruct.Mapper;
+
+import com.cooksys.twitter_api.dtos.HashtagDto;
+import com.cooksys.twitter_api.entities.Hashtag;
+
+
+@Mapper(componentModel = "spring")
+public interface HashtagMapper {
+
+	HashtagDto entityToDto(Hashtag entity);
+}
