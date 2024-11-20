ccf3d0d56c3475f3a750ca777cc5c533e1ae238c
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-11-20 09:55:12 -0600

    Update UserMapper.java

--- twitter_api/src/main/java/com/cooksys/twitter_api/mappers/UserMapper.java
+++ twitter_api/src/main/java/com/cooksys/twitter_api/mappers/UserMapper.java
@@ -7,8 +7,11 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class })
 public interface UserMapper {
+
+    @Mapping(target = "username", source = "credentials.username")
+    UserResponseDto entityToDto(User user);
 
     User DtoToEntity(User user);
 
