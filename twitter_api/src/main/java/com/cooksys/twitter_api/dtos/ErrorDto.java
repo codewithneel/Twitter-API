ca95036ee41343c30e1bfae433ccbd0973276b5b
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-11-20 09:45:27 -0600

    Update ErrorDto.java

--- twitter_api/src/main/java/com/cooksys/twitter_api/dtos/ErrorDto.java
+++ twitter_api/src/main/java/com/cooksys/twitter_api/dtos/ErrorDto.java
@@ -2,22 +2,12 @@ package com.cooksys.twitter_api.dtos;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ErrorDto {
-
-	public ErrorDto(String message2) {
-		message = message2;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
 
 	private String message;
 }
